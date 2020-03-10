af7a18b5104a2d41622ab2598f6e9f2b72cba2c5
Federico <federico.caiello@gmail.com>
2020-03-10 17:33:28 +0100

    magazzino

--- prodotti.php
+++ prodotti.php
@@ -1,10 +1,14 @@
 <?php
-  class prodotti {
+  class Magazzino {
+    public $genere;
+  }
+
+  class Disinfettanti extends Magazzino{
     public $isbn;
     public $prodotto;
     public $prezzo;
-    public $genere;
     public $scaffale;
+    public $quantita;
     public function __construct($_isbn, $_prodotto) {
       $this->isbn=$_isbn;
       $this->prodotto=$_prodotto;
@@ -16,13 +20,41 @@
       return $this->scaffale;
     }
   }
-
-  $amuchina = new Prodotti('ISBN084', 'Amuchina');
-  $amuchina->prezzo=10.00;
+  class Oggetti extends Magazzino{
+    public $isbn;
+    public $prodotto;
+    public $prezzo;
+    public $scaffale;
+    public $quantita;
+    public function __construct($_isbn, $_prodotto) {
+      $this->isbn=$_isbn;
+      $this->prodotto=$_prodotto;
+    }
+    public function getPosition() {
+      if (empty($this->scaffale)) {
+        die ('Lo scaffale non e\' stato inserito');
+      }
+      return $this->scaffale;
+    }
+  }
+ // esempio con amuchina
+  $amuchina = new Disinfettanti('ISBN084', 'Amuchina');
+  $amuchina->prezzo=10;
+  $amuchina->scaffale=13 . ' ' . b;
+  $amuchina->quantita= '20 litri';
   $amuchina->genere= 'Disinfettante';
-  $amuchina->scaffale=10;
-  echo $amuchina->getPosition();
+
+  
 
   var_dump($amuchina);
+
+  $materassi = new Oggetti('ISBN22', 'Materasso');
+  $materassi->prodotto= 'materasso';
+  $materassi->prezzo= 150;
+  $materassi->scaffale= 18 . ' ' . c;
+  $materassi->quantita= 300 . ' ' . 'pezzi';
+  $materassi->genere= 'prodotti per la salute';
+
+  var_dump($materassi);
 
   ?>
